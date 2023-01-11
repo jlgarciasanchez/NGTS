8b831f6efb0e476d329a31171ef0051c1fee22df
HIBERUS\jlgarcias <jlgarcias@hiberus.com>
2023-01-11 16:26:24 +0100

    first commit

---
+++ path-optimizer/src/test/java/ngts/pathoptimizer/PathOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ngts.pathoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
